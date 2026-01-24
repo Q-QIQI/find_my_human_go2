3bb06521ee7377a34c5f6726576318b5d9d4d43e
arpan <arpanekka.a1.73@gmail.com>
2026-01-24 16:18:40 +0100

    Initial commit: human_tracker + realsense-ros source code

---
+++ human_tracker/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'human_tracker'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/human_tracker_launch.py']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Arpan',
+    maintainer_email='arpan@example.com',
+    description='YOLOv8 human detection and target locking with RealSense D435i',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'detector = human_tracker.human_detector:main',
+        ],
+    },
+)
